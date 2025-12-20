afe065f4ec60a803fa0274faf6e7d0f388e89a94
aziz tbk <mohamedaziz.tabakh@esen.tn>
2025-12-20 22:54:55 +0100

    Update data.js

--- js/data.js
+++ js/data.js
@@ -5,54 +5,87 @@ const studentsData = [
         initials: "AT",
         level: "L2BI",
         skills: ["Programation", "Statistiques", "Bases de Données"],
-        projectInterest: "Web Programming 1",
+        projectInterest: "Statistiques Inférentielles",
         bio: "Passionate about clean code and data-driven solutions",
         email: "aziz@example.com",
         facebook: "https://www.facebook.com/aziz.tabakh.58/"
     },
     {
         id: 2,
-        name: "Marie Dubois",
-        initials: "MD",
-        level: "M1",
+        name: "Wissal Jaafer",
+        initials: "WJ",
+        level: "L2BI",
         skills: ["Research", "Presenting", "Marketing"],
-        projectInterest: "OOP Programming",
+        projectInterest: "Marketing Digital",
         bio: "Focused on academic excellence and thorough analysis",
         email: "marie@example.com",
-        facebook: "https://facebook.com/marie"
+        facebook: "https://www.facebook.com/wissal.jaafer.3"
     },
     {
         id: 3,
-        name: "James Taylor",
-        initials: "JT",
+        name: "Yassine Rachdi",
+        initials: "YR",
         level: "L2BIS",
         skills: ["Design", "Programation", "Presenting"],
-        projectInterest: "Statistiques Inférentielles",
+        projectInterest: "Web Programming 1",
         bio: "Creating beautiful and functional user experiences",
         email: "james@example.com",
-        facebook: "https://facebook.com/james"
+        facebook: "https://www.facebook.com/yassine.rachdi.18"
     },
     {
         id: 4,
-        name: "Sofia Rodriguez",
-        initials: "SR",
-        level: "L3EB",
+        name: "Brahmy Hiba",
+        initials: "BH",
+        level: "L2BIS",
         skills: ["Marketing", "Math", "Research"],
-        projectInterest: "Fondements de l’IA",
+        projectInterest: "Social Media & Community Management",
         bio: "Bridging technology and business strategy",
         email: "sofia@example.com",
-        facebook: "https://facebook.com/sofia"
+        facebook: "https://www.facebook.com/brahmy.hiba"
     },
     {
         id: 5,
-        name: "Liam O'Brien",
-        initials: "LO",
-        level: "L3BIS",
+        name: "Benzarti Amir",
+        initials: "BA",
+        level: "M1",
         skills: ["Programation", "Dev", "Marketing"],
-        projectInterest: "Marketing Digital",
+        projectInterest: "OOP Programming",
         bio: "Building robust and scalable applications",
         email: "liam@example.com",
-        facebook: "https://facebook.com/liam"
+        facebook: "https://www.facebook.com/benzartiamir30"
+    },
+    {
+        id: 6,
+        name: "Khaled Abdellatif",
+        initials: "KA",
+        level: "M1",
+        skills: ["Programation", "Dev", "Marketing"],
+        projectInterest: "Conception OO des Systèmes d’Information",
+        bio: "Building robust and scalable applications",
+        email: "liam@example.com",
+        facebook: "https://www.facebook.com/khaled.abdellatif.37604"
+    },
+    {
+        id: 7,
+        name: "Iyed Nasra",
+        initials: "IN",
+        level: "L1",
+        skills: ["Programation", "Dev", "Marketing"],
+        projectInterest: "Fondements de l’IA",
+        bio: "Building robust and scalable applications",
+        email: "liam@example.com",
+        facebook: "https://www.facebook.com/iyednasra"
+    },
+    {
+        id: 8,
+        name: "Roudayna Hazgui",
+        initials: "RH",
+        level: "L1",
+        skills: ["Programation", "Dev", "Marketing"],
+        projectInterest: "Économie Numérique",
+        bio: "Building robust and scalable applications",
+        email: "liam@example.com",
+        facebook: "https://www.facebook.com/roudayna.hazgui.71"
     }
 ];
 
